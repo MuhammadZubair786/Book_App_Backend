f4a203fd1b1b743ac779509b797802a42d01f5f2
MuhammadZubair786 <mzhassan444@gmail.com>
2024-01-08 19:05:36 +0500

    update

--- constant/user.js
+++ constant/user.js
@@ -6,7 +6,7 @@ const seckret_key = process.env.seckret_key;
 exports.TokenGenerate=(userId,res)=>{
     console.log("test token")
     const token = jwt.sign({ userId: userId }, seckret_key, { expiresIn: '1h' });
-    res.set("authtoken", token);
+    // res.set("authtoken", token);
    return token
 
 }
